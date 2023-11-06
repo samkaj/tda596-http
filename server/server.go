0564e9773364fdc3d61cd41ae31631f226501a96
samkaj <kajavasamuel@gmail.com>
2023-11-06 14:57:08 +0100

    Resolve paths correctly.
    Co-authored-by: Dennis Sehic <Urbanthesecond@users.noreply.github.com>
    Co-authored-by: Daniel Rygaard <Danryg@users.noreply.github.com>

--- server/server.go
+++ server/server.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"net/http"
 	"path"
+	"path/filepath"
 	"strings"
 	"time"
 )
@@ -60,7 +61,7 @@ func (s *Server) Serve() error {
 	}
 }
 
-// Given an established connection to a client, this method will handles incoming HTTP requests from the client
+// Handles incoming HTTP requests from client connection.
 func (s *Server) HandleConnection(conn net.Conn) error {
 	log.Printf("handling connection from %s\n", conn.LocalAddr().String())
 	time.Sleep(time.Millisecond * 500)
@@ -125,13 +126,17 @@ func (s *Server) HandleGet(req *http.Request, res *http.Response) {
 		s.HandleBadRequest(req, res)
 		return
 	}
-	// Initialize the header
-	res.Header = make(http.Header)
+
 	// Set the header content type based on the extension that has been requested
+	res.Header = make(http.Header)
 	res.Header.Set("Content-Type", contentType)
 
-	// TODO: Fix me
-	path := fmt.Sprintf("/Users/samkaj/code/dist/http-lab/fs%s", req.URL.Path) // FIXME: absolute path...
+	// Put together the path to the file with path.Join
+	path := filepath.Join("fs", req.URL.Path)
+	if err != nil {
+		s.HandleNotFound(res)
+		return
+	}
 	data, err := GetFile(path)
 	if err != nil {
 		s.HandleNotFound(res)
@@ -141,17 +146,18 @@ func (s *Server) HandleGet(req *http.Request, res *http.Response) {
 	res.Body = CreateBody(string(data))
 }
 
-// Handles HTTP PUT requests.
+// Handles HTTP POST requests.
 func (s *Server) HandlePost(req *http.Request, res *http.Response) {
-	// Get file name from URL param
-	filename := req.URL.Query().Get("filename")
+	// Get filename and path
+	filename := path.Base(req.URL.Path)
+	path := filepath.Join("fs", filename)
+
 	// Get file data
 	data, err := io.ReadAll(req.Body)
 	if err != nil {
 		s.HandleBadRequest(req, res)
 	}
 	// Write to fs
-	path := fmt.Sprintf("/Users/samkaj/code/dist/http-lab/fs/%s", filename) // FIXME: absolute path...
 	WriteFile(path, data)
 
 	// Build response
