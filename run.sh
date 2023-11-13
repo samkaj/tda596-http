118f326e9c61c5bf1ad6edecdd0820a774d99924
samkaj <kajavasamuel@gmail.com>
2023-11-13 17:20:23 +0100

    Cleanup binaries and unused script

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build --rm --tag http-server .
-
-# TODO: when server is listening, uncomment below
-# docker run -p 8080:8080 http-server
-
-docker run http-server # TODO: and remove this line
